8ef5263e8b46925a2c94b886e418891759a702e3
Sonja Heinze <Skippy@sonja-heinzes-imac.local>
2020-06-02 15:26:09 +0200

    Escoria Update Branch

--- device/globals/bg_music.gd
+++ device/globals/bg_music.gd
@@ -1,41 +1,36 @@
-extends Control
+extends "res://globals/item.gd"
 
 var stream
-
-var state = "default"
-
-export var global_id = "bg_music"
+var current_music
 
 func game_cleared():
 	set_state("off", true)
-	self.disconnect("tree_exited", vm, "object_exit_scene")
-	vm.register_object(global_id, self)
+	if global_id != "":
+		vm.register_object(global_id, self)
 
 func set_state(p_state, p_force = false):
-	# If already playing this stream, keep playing, unless p_force
-	if p_state == state and not p_force and stream.is_playing():
+
+	if p_state == state && !p_force && stream.is_playing():
 		return
 
-	state = p_state
+	.set_state(p_state, p_force)
 
-	# If state is "off"/"default", turn off music
-	if state == "off" or state == "default":
-		stream.stream = null
+	if stream == null:
 		return
 
-	var resource = load(p_state)
+	if state == "off" || state == "default":
+		stream.set_stream(null)
+		return
 
-	stream.stream = resource
-
-	if stream.stream:
-		resource.set_loop(true)
+	var res = load(p_state)
+	stream.set_stream(res)
+	if res != null:
+		stream.set_loop(true)
 		stream.play()
-		stream.volume_db = vm.settings.music_volume
+		stream.set_volume(vm.settings.music_volume)
+
+
 
 func _ready():
-	stream = $"stream"
-
-	vm.register_object(global_id, self)
-
-	add_to_group("game")
+	stream = get_node("stream")
 
--- device/ui/verb_menu.gd
+++ device/ui/verb_menu.gd
@@ -1,14 +1,18 @@
 extends Control
 
+var vm
+
 var act_buttons = []
 
 func action_changed(action):
-	vm.set_current_action(action)
+	get_tree().call_group("game", "set_current_action", action)
 
 	for b in act_buttons:
 		b.set_pressed(b.get_name() == action)
 
 func _ready():
+
+	vm = get_node("/root/vm")
 
 	var acts = get_node("actions")
 	for i in range(acts.get_child_count()):
@@ -18,4 +22,5 @@ func _ready():
 
 		c.connect("pressed", self, "action_changed", [c.get_name()])
 		act_buttons.push_back(c)
+
 
