b79c8cc8579ce4ba91afc0262e1a42cd2963a001
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2020-06-21 18:41:33 +0200

    Add missing dialog lines, collectable letters

--- device/scenes/00_trailer/scene.gd
+++ device/scenes/00_trailer/scene.gd
@@ -1,1 +1,13 @@
 extends "res://globals/scene_base.gd"
+
+func _ready():
+	var vm = get_tree().get_root().get_node("vm")
+	if has_node("collect_label"):
+		if vm.get_global("jacques_collected_letters"):
+			get_node("collect_label").hide()		
+			get_node("letter_l").hide()		
+			get_node("letter_u").hide()	
+		elif vm.get_global("jacques_first_call"):
+			get_node("collect_label").show()		
+			get_node("letter_l").show()		
+			get_node("letter_u").show()	
